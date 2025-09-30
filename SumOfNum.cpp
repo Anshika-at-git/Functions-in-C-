c6c7f1b8ff94e7f0a992d48adad31020eead3c0a
Anshika Singh <anshikasingh082005@gmail.com>
2025-09-30 23:26:54 +0530

    Add files via upload

---
+++ SumOfNum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sumOfNum(int n){
+    int sum = 0;
+    for(int i=1; i<=n; i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+int main(){
+    cout<< sumOfNum(10)<< endl;
+    cout<< sumOfNum(20)<< endl;
+    return 0;
+}
